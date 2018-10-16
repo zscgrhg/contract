17da1ee617f53d0532ccfacc93f724cc3e3959b4
zscgrhg <zscgrhg@qq.com>
2018-10-16 23:46:45 +0800

    sn

--- src/main/java/com/zte/crm/framework/contract/processor/CopyDefaultProcessor.java
+++ src/main/java/com/zte/crm/framework/contract/processor/CopyDefaultProcessor.java
@@ -6,6 +6,7 @@ import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeTranslator;
+import com.sun.tools.javac.util.ListBuffer;
 import com.zte.crm.framework.contract.ProcessorSupport;
 import com.zte.crm.framework.contract.annotation.Contract;
 import com.zte.crm.framework.contract.annotation.CopyDefault;
@@ -16,7 +17,9 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -34,23 +37,15 @@ public class CopyDefaultProcessor extends ProcessorSupport {
             Tree.Kind kind = jct.getKind();
             if (INTERFACE == kind) {
                 Type.ClassType clazz = (Type.ClassType) tc.asType();
-                List<Type> contracts = clazz.all_interfaces_field.stream()
-                        .flatMap(f -> {
-                            Type.ClassType tcf = (Type.ClassType) f;
-                            if (tcf.all_interfaces_field != null && tcf.all_interfaces_field.length() > 0) {
-                                return Stream.of(tcf.all_interfaces_field.stream(), Stream.of(tcf))
-                                        .flatMap(t -> t);
-                            } else {
-                                return Stream.of(tcf);
-                            }
-                        })
-                        .filter(f -> f.tsym.getAnnotationsByType(Contract.class).length > 0)
-                        .collect(Collectors.toList());
+
+                ListBuffer<Type.ClassType> contracts = new ListBuffer<>();
+                Set<String> key = new HashSet<>();
+                filter(contracts, clazz.all_interfaces_field);
                 jct.accept(new TreeTranslator() {
                     @Override
                     public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
                         super.visitClassDef(jcClassDecl);
-                        contracts.forEach(c -> copyMethod((Type.ClassType) c, jcClassDecl));
+                        contracts.forEach(c -> copyMethod(c, jcClassDecl, key));
                     }
                 });
             }
@@ -59,11 +54,32 @@ public class CopyDefaultProcessor extends ProcessorSupport {
         return true;
     }
 
-    private void copyMethod(Type.ClassType from, JCTree.JCClassDecl to) {
+    private void filter(ListBuffer<Type.ClassType> buffer, List<Type> source) {
+        //f.tsym.getAnnotationsByType(Contract.class).length > 0
+        if (source.isEmpty()) {
+            return;
+        }
+        source.stream()
+                .filter(f -> f.tsym.getAnnotationsByType(Contract.class).length > 0)
+                .map(f -> (Type.ClassType) f)
+                .forEach(buffer::append);
+        List<Type> con = source.stream()
+                .map(f -> (Type.ClassType) f)
+                .flatMap(f ->
+                        Optional.ofNullable(f.all_interfaces_field)
+                                .map(t -> t.stream())
+                                .orElse(Stream.empty())
+                )
+                .collect(Collectors.toList());
+        filter(buffer, con);
+    }
+
+    private void copyMethod(Type.ClassType from, JCTree.JCClassDecl to, Set<String> key) {
         List<Symbol> enclosedElements = from.tsym.getEnclosedElements();
         enclosedElements.stream()
                 .filter(e -> e instanceof Symbol.MethodSymbol)
                 .map(e -> (Symbol.MethodSymbol) e)
+                .filter(e -> !key.contains(e.toString()))
                 //.filter(e -> e.isDefault())
                 .forEach(msym -> {
                     Symbol.MethodSymbol copyed
@@ -73,6 +89,7 @@ public class CopyDefaultProcessor extends ProcessorSupport {
                     copyed.savedParameterNames = msym.savedParameterNames;
                     copyed.appendAttributes(msym.getDeclarationAttributes());
                     to.defs = to.defs.prepend(make.MethodDef(copyed, null));
+                    key.add(msym.toString());
                 });
         System.out.println(enclosedElements);
     }
